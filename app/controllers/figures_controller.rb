6c4377e8830096543c0b0862e04bb646f92a6b76
Randall Harris Haynes <harrishaynes@gmail.com>
2018-03-29 01:53:08 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,2 +1,45 @@
 class FiguresController < ApplicationController
+  
+    get "/figures" do
+      @figures = Figure.all
+      erb :"figures/index"
+    end
+
+    get "/figures/new" do
+      erb :"figures/new"
+    end
+    
+    post "/figures" do
+      figure = Figure.new(params["figure"])
+      if !!figure.save
+        figure.landmarks << Landmark.create(params["landmark"]) unless params["landmark"]["name"].empty?
+        figure.titles    <<    Title.create(params["title"])    unless params["title"]["name"].empty?
+        figure.save
+        redirect "/figures/#{figure.id}"
+      else
+        redirect "/figures/new"
+      end
+    end
+
+    get "/figures/:id" do
+      @figure = Figure.find_by(:id => params["id"])
+      erb :"figures/show"
+    end
+
+    get "/figures/:id/edit" do
+      @figure = Figure.find_by(:id => params["id"])
+      erb :"figures/edit"
+    end
+
+    patch "/figures/:id" do
+      figure = Figure.find_by(:id => params["id"])
+      if !!figure.update(params["figure"])
+        figure.landmarks << Landmark.create(params["landmark"]) unless params["landmark"]["name"].empty?
+        figure.titles    <<    Title.create(params["title"])    unless params["title"]["name"].empty?
+        figure.save
+        redirect "/figures/#{figure.id}"
+      else
+        redirect "/figures/#{figure.id}/edit"
+      end
+    end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,2 +1,49 @@
 class LandmarksController < ApplicationController
+  
+  get "/" do
+    redirect "/landmarks"
+  end
+
+  get "/landmarks" do
+    @landmarks = Landmark.all
+    @figures   = Figure.all
+    erb :"landmarks/index"
+  end
+
+  get "/landmarks/new" do
+    erb :"landmarks/new"
+  end
+  
+  post "/landmarks" do
+    landmark = Landmark.new(
+      :name           => params["landmark"]["name"],
+      :year_completed => params["landmark"]["year_completed"].to_i
+      )
+    if !!landmark.save
+      redirect "/landmarks"
+    else
+      redirect "/landmarks/new"
+    end
+  end
+
+  get "/landmarks/:id" do
+    @landmark = Landmark.find_by(:id => params["id"])
+    erb :"landmarks/show"
+  end
+
+  get "/landmarks/:id/edit" do
+    @landmark = Landmark.find_by(:id => params["id"])
+    erb :"landmarks/edit"
+  end
+
+  patch "/landmarks/:id" do
+    landmark = Landmark.find_by(:id => params["id"])
+    landmark.name           = params["landmark"]["name"]
+    landmark.year_completed = params["landmark"]["year_completed"]
+    if !!landmark.save
+      redirect "/landmarks/#{landmark.id}"
+    else
+      redirect "/landmarks/#{landmark.id}/edit"
+    end
+  end
 end
